414060ecc3320cb2742b9aa28fe5688ae9696e62
johannag285 <johannag285@gmail.com>
2018-09-05 18:15:16 -0500

    Clase coordinador entity

--- src/main/java/edu/konrad/registroAcademico/entities/CoordinadorEntity.java
+++ src/main/java/edu/konrad/registroAcademico/entities/CoordinadorEntity.java
@@ -5,10 +5,48 @@
  */
 package edu.konrad.registroAcademico.entities;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 /**
- *
- * @author johan
+ *Clase encargada de modelar la tabla Coordinador
+ * @author Johanna G.
  */
-public class CoordinadorEntity {
+@Entity (name="Coordinador" )
+public class CoordinadorEntity  implements Serializable {
+    /**
+     * Llave priamria de la tabla Coordinador
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_coordinador;
+    
+    /**
+     * Atributo que hace referencia a la columna del código del coordinador
+     */
+    @Column
+    private String cod_coordinador;
+    
+    /**
+     * Atributo que hace referencia a la llave foranea de la tabla Persona
+     */
+    @Column
+    @OneToMany
+    private Long persona_id;
+    
+    /**
+     * Constructor de entidad de coordiandor
+     */
+
+    public CoordinadorEntity() {
+    }
+    
+    
+    
     
 }
