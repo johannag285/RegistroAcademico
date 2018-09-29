3b87ef052e0d34f6ea12aede6b7f9ca29448fab0
johannag285 <johannag285@gmail.com>
2018-09-29 14:07:45 -0500

    Se agrego el paquete resources, la clase PersonaResources y la clase AplicationConfig

---
+++ src/main/java/edu/konrad/registroAcademico/resources/AplicationConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.konrad.registroAcademico.resources;
+
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Clase para el registro de recursos
+ * @author johanna
+ */
+
+@ApplicationPath("/api")
+public class AplicationConfig  extends Application{
+    
+    @Override
+    public Set<Class<?>> getClasses(){
+        Set<Class<?>> resources = new HashSet<Class<?>>();
+        return resources;
+    }
+    
+    private void addResourcesClasses(Set<Class<?>> resources){
+        resources.add(PersonaResources.class);
+    }
+}
